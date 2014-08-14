6407006d1474978c7abde1cdc627c3f7b4075fa2
luizventurote <luiz_17@ymail.com>
2014-08-13 23:11:03 -0300

    First files.

---
+++ build/js/elements/menu.js
@@ -0,0 +1,26 @@
+/**
+ *  Menu
+ */
+
+$(document).ready(function(){ 
+	var touch 	= $('#touch-menu');
+	var menu 	= $('#main-menu');
+ 
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+
+	// Buttom
+	$("#touch-menu").click(function(){
+		$(this).toggleClass("active");
+	});
+	
+});
---
+++ index.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+
+        <meta charset="UTF-8">
+        <title>Luiz Venturote // Front-End Developer</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Icons -->
+        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/apple-touch-icon.png">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+
+        <!-- Style -->
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.min.css">
+
+        <!-- Modernizr -->
+        <script src="<?php echo get_template_directory_uri(); ?>/js/lib/modernizr-2.6.2.min.js"></script>
+
+        <?php wp_head(); ?>
+
+    </head>
+    <body>
+
+        <header>
+
+            <menu class="menu main-menu row" id="main-menu">          
+                <nav class="container">
+                    <ul class="col-sm-12">
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Talks</a></li>
+                        <li><a href="#">Posts</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Github</a></li>
+                    </ul>
+                </nav>
+            </menu>
+
+            <div class="main-header row">
+
+                <div class="container">
+
+                    <div class="col-sm-12 header-container">
+
+                        <div class="menu menu-btn" id="touch-menu"><span></span></div>
+
+                        <a href="#" class="logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Luiz Venturote // Front-End Developer">
+                            <h1>Luiz Venturote // Front-End Developer</h1>
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </header>
+
+        <main class="row">
+
+            <section class="container">
+
+                <article class="col-sm-12 post">
+                    
+                    <h2>My personal portfolio</h2>
+
+                    <div class="divider divider-single"></div>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quisquam, quas excepturi eum enim error. Molestias eveniet fugit accusantium velit? Neque, quos odio eos itaque harum cupiditate? Debitis, eos, tempore....</p>
+
+                    <div class="post-info">   
+
+                        <span class="post-time">
+                            <i class="fa fa-clock-o icon"></i>
+                            <time  datetime="2013-11-18 11:52"> 18 de Novembro de 2013</time>
+                        </span>
+
+                        <span class="post-tags">
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Acessibilidade</a>,
+                            <a href="#">Wordpress</a>
+                        </span>
+
+                    </div>
+
+                </article>
+
+                <article class="col-sm-12 post">
+                    
+                    <a href="#" class="post-title"><h2>My personal portfolio</h2></a>
+
+                    <div class="divider divider-single"></div>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quisquam, quas excepturi eum enim error. Molestias eveniet fugit accusantium velit? Neque, quos odio eos itaque harum cupiditate? Debitis, eos, tempore....</p>
+
+                    <div class="post-info">   
+
+                        <span class="post-time">
+                            <i class="fa fa-clock-o icon"></i>
+                            <time  datetime="2013-11-18 11:52"> 18 de Novembro de 2013</time>
+                        </span>
+
+                        <span class="post-tags">
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Acessibilidade</a>,
+                            <a href="#">Wordpress</a>
+                        </span>
+
+                    </div>
+
+                </article>
+
+                <div class="col-sm-4">
+                    <div class="media-box">
+
+                        <div class="media-thumbnail">
+
+                            <div class="fadder-box">
+                                <div class="fadder-image">
+                                    <a href="#">
+                                        <img src="img/photos/jesus-loves.jpg" alt="Jesus Loves">
+                                        <div class="fadder-background"></div>
+                                        <div class="fadder-ico"></div>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <a href="#" class="media-title">Title example</a>
+
+                        <div class="media-info">
+
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Category name</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="media-box">
+
+                        <div class="media-thumbnail">
+
+                            <div class="fadder-box">
+                                <div class="fadder-image">
+                                    <a href="#">
+                                        <img src="img/photos/jesus-loves.jpg" alt="Jesus Loves">
+                                        <div class="fadder-background"></div>
+                                        <div class="fadder-ico"></div>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <a href="#" class="media-title">Title example</a>
+
+                        <div class="media-info">
+
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Category name</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="media-box">
+
+                        <div class="media-thumbnail">
+
+                            <div class="fadder-box">
+                                <div class="fadder-image">
+                                    <a href="#">
+                                        <img src="img/photos/jesus-loves.jpg" alt="Jesus Loves">
+                                        <div class="fadder-background"></div>
+                                        <div class="fadder-ico"></div>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <a href="#" class="media-title">Title example</a>
+
+                        <div class="media-info">
+
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Category name</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="media-box">
+
+                        <div class="media-thumbnail">
+
+                            <div class="fadder-box">
+                                <div class="fadder-image">
+                                    <a href="#">
+                                        <img src="img/photos/jesus-loves.jpg" alt="Jesus Loves">
+                                        <div class="fadder-background"></div>
+                                        <div class="fadder-ico"></div>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <a href="#" class="media-title">Title example</a>
+
+                        <div class="media-info">
+
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Category name</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="media-box">
+
+                        <div class="media-thumbnail">
+
+                            <div class="fadder-box">
+                                <div class="fadder-image">
+                                    <a href="#">
+                                        <img src="img/photos/jesus-loves.jpg" alt="Jesus Loves">
+                                        <div class="fadder-background"></div>
+                                        <div class="fadder-ico"></div>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <a href="#" class="media-title">Title example</a>
+
+                        <div class="media-info">
+
+                            <i class="fa fa-tag icon"></i>
+                            <a href="#">Category name</a>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-sm-12">
+                
+                    <div class="loading"><i class="fa fa-spinner fa-spin icon"></i> Loading more posts...</div>
+
+                </div>
+
+            </section>
+
+        </main>
+
+        <footer class="footer-main row">
+
+            <div class="container">
+
+                <div class="col-sm-12">
+
+                    <span class="copyright">By Luiz Venturote <i class="fa fa-code icon"></i> Fork me on <a href="#">GitHub</a>.</span>
+
+                </div>
+
+            </div>
+            
+        </footer> 
+        
+        <script src="<?php echo get_template_directory_uri(); ?>/js/main.min.js"></script>
+
+        <?php wp_footer(); ?>
+
+    </body>
+</html>
